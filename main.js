e0bf33802de3d4ed982f4b865864d4d059d82ec3
minseung.kwon <minseung.kwon@rooffunding.com>
2018-06-07 16:11:12 +0900

    공부중 커밋ㄱ

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
 var express = require('express');
 var app = express();
+var user = require('./routes/user')
+var morgan = require('morgan')
+var bodyParser = require('body-parser')
 
-app.get('/', function(req, res) {
-    res.send('Hello World')
-});
+// var myLogger = function(req, res, next) {
+//     console.log(req.url);
+//     next();
+// };
+//
+// app.use(myLogger);
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use('/', express.static('public'));
+app.use('/user', user);
 
-app.get('/user/:id', function(req, res) {
-    res.send('Received a GET request, param:' + req.params.id);
-});
-
-app.post('/user', function(req, res) {
-    res.json({ success: true })
-});
-
-app.put('/user', function(req, res) {
-    res.status(400).json({ message: 'Hey, you. Bad Request!' });
-});
-
-app.delete('/user', function(req, res) {
-    res.send('Received a DELETE request');
-});
-
-app.listen(5000, function () {
-    console.log('Example App is listening on port 5000');
+app.listen(10000, function () {
+    console.log('Example App is listening on port 10000');
 });
--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,15 @@ var express = require('express')
 var router = express.Router();
 
 router.get('/:id', function(req, res) {
-    res.send('Hello World')
-});
-
-router.get('/:id', function(req, res) {
     res.send('Received a GET request, param:' + req.params.id);
 });
 
 router.post('/', function(req, res) {
-    res.json({ success: true })
+    console.log(JSON.stringify(req.body, null, 2));
+    res.json({
+        success: true,
+        user: req.body.username
+    })
 });
 
 router.put('/', function(req, res) {
@@ -20,3 +20,5 @@ router.put('/', function(req, res) {
 router.delete('/', function(req, res) {
     res.send('Received a DELETE request');
 });
+
+module.exports = router;
